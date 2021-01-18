80b70a463a83165896b5e24e2177aa87b24d0011
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-18 22:24:26 +0000

    Using render dir

--- .init-scripts/make-build-tools.sh
+++ .init-scripts/make-build-tools.sh
@@ -2,5 +2,11 @@
 
 set -o errexit
 
+mkdir build  
+cd build
 git clone --single-branch --branch external-site https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
 .scripts/install-dependencies.sh
+             
+             
+             
+             
--- .init-scripts/make-distclean.sh
+++ .init-scripts/make-distclean.sh
@@ -2,5 +2,6 @@
 
 set -o errexit
 
-rm -rf source-repo branches tags index.md index-contents.md
-rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
+rm -rf build/
+#rm -rf source-repo branches tags index.md index-contents.md
+#rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
